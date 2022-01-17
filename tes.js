b93f586430d92365801223cfa79e78493956600c
Zenxlay <fendersiucul2@gmail.com>
2022-01-17 14:27:48 +0700

    tugas js pertama

---
+++ tes.js
@@ -0,0 +1,24 @@
+function perkenalan(){
+    console.log("Hi namaku adalah saya");
+}
+perkenalan()
+
+function convertyear(year){
+    console.log(year * 365, "Hari");
+}
+convertyear(1)
+
+function convertday(day){
+    console.log(day * 24, "Jam");
+}
+convertday(1)
+
+function converthour(hour){
+    console.log(hour * 60, "Menit");
+}
+converthour(1)
+
+function convertmin(min){
+    console.log(min * 60, "Detik");
+}
+convertmin(1)
